dff06951a5e5c7e5f90688b636a20542e4a427df
Jorge Alejandro Diez <alejandro@latindigital.io>
2021-04-20 15:31:38 -0500

    Update index.js

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,21 +2,21 @@ export default {
   colors: {
     text: '#000',
     background: '#fff',
-    primary: '##5C2941',
+    primary: '#5C2941',
     accent: '#fff',
     muted: 'rgba(0, 0, 0, 0.7)',
     cardBg: '#fff',
-    borderColor:"##5C2941",
+    borderColor:"#540229",
     labelText: "#777",
     inputBorder: "#aaa",
     inputBackground: "#fff",
-    socialIcons:"#5C2941",
+    socialIcons:"#a8959e",
     socialIconsHover:"#5c2941",
     modes: {
       dark: {
         text: '#f5f5f5',
         background: '#111',
-        primary: '#5C2941',
+        primary: '#252525',
         accent: '#5C2941',
         muted: 'rgba(255, 255, 255, 0.7)',
         buttonAccent: '#fff',
@@ -39,7 +39,7 @@ export default {
     },
     button: {
       bg: 'buttonAccent',
-      color: 'Accent'
+      color: 'accent'
     }
   }
 }
